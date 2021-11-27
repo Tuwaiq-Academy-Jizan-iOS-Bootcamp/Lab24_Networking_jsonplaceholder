e277734a62918b1801e64d4bcd80b7c7c63d9de1
ArwaAlattas <Arwa.Nabeel.jiz.s@tuwaiq.edu.sa>
2021-11-27 03:39:14 +0300

    ArwaAlattas

---
+++ ArwaAlattas_Lab24_Networking_jsonplaceholder/ArwaAlattas_Lab24_Networking_jsonplaceholder/View/PersonCell.swift
@@ -0,0 +1,31 @@
+//
+//  PersonCell.swift
+//  ArwaAlattas_Lab24_Networking_jsonplaceholder
+//
+//  Created by Arwa Alattas on 22/04/1443 AH.
+//
+
+import Foundation
+import UIKit
+class PersonCell:UITableViewCell{
+    
+    
+    @IBOutlet weak var personName: UILabel!
+    
+    @IBOutlet weak var personID: UILabel!
+    
+    @IBOutlet weak var personUserName: UILabel!
+    
+    @IBOutlet weak var persomEmail: UILabel!
+    
+    
+    @IBOutlet weak var personAddrees: UILabel!
+    
+    
+    @IBOutlet weak var personPhone: UILabel!
+    
+    
+    @IBOutlet weak var personWebsite: UILabel!
+    
+    @IBOutlet weak var personCompany: UILabel!
+}
