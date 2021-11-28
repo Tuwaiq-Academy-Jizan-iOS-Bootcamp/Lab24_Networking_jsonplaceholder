fe97831fccf48a779d1f89ab93d7d89a036c8e41
laila hakami <laila.hakami.jiz.s@tuwaiq.edu.sa>
2021-11-28 17:53:32 +0300

    laylahakami_lab24

---
+++ NetworkingJsonLab24/NetworkingJsonLab24/View/DetilsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  DetilsTableViewCell.swift
+//  NetworkingJsonLab24
+//
+//  Created by layla hakami on 22/04/1443 AH.
+//
+
+import Foundation
+import UIKit
+class DetilsTableViewCell:UITableViewCell{
+    
+    @IBOutlet weak var idLabelTableViewCell: UILabel!
+    
+    @IBOutlet weak var nameLabelTableViewCell: UILabel!
+    
+    @IBOutlet weak var userNameLabelTableViewCell: UILabel!
+    
+    
+    @IBOutlet weak var emailLabelTableViewCell: UILabel!
+    
+    
+    @IBOutlet weak var addresslLabelTableViewCell: UILabel!
+    
+    
+    @IBOutlet weak var phonelLabelTableViewCell: UILabel!
+    
+    
+    @IBOutlet weak var websitelLabelTableViewCell: UILabel!
+    
+    @IBOutlet weak var companylLabelTableViewCell: UILabel!
+    
+}
