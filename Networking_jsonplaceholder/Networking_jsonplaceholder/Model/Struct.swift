ceb3a319ee2f774d612761814b04c5cac1f41d93
Abdulrahman Gahzwni <Abdulrahman.gahzwni.jiz.s@tuwaiq.edu.sa>
2021-11-28 09:24:51 +0300

    Gahzwni -> lab 24

---
+++ Networking_jsonplaceholder/Networking_jsonplaceholder/Model/Struct.swift
@@ -0,0 +1,8 @@
+//
+//  Struct.swift
+//  Networking_jsonplaceholder
+//
+//  Created by Abdulrahman Gazwani on 22/04/1443 AH.
+//
+
+import Foundation
---
+++ Networking_jsonplaceholder/Networking_jsonplaceholderUITests/Networking_jsonplaceholderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Networking_jsonplaceholderUITestsLaunchTests.swift
+//  Networking_jsonplaceholderUITests
+//
+//  Created by Abdulrahman Gazwani on 22/04/1443 AH.
+//
+
+import XCTest
+
+class Networking_jsonplaceholderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
