2d72b387f060a8d0eb7bc0fadeca972befa20885
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-12-01 10:50:02 +0300

    bushra_24

--- Bushra Barakat_Lab24/Bushra Barakat_Lab24/Controller/ViewController.swift
+++ Bushra Barakat_Lab24/Bushra Barakat_Lab24/Controller/ViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var idFirstLable: UILabel!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var firstUserNameLabel: UILabel!
+    @IBOutlet weak var firstEmailLabel: UILabel!
+    @IBOutlet weak var firstAderssStreetLabel: UILabel!
+    @IBOutlet weak var firstAddressSuiteLabel: UILabel!
+    @IBOutlet weak var firstAddressCityLabel: UILabel!
+    @IBOutlet weak var firstAddressZipLabel: UILabel!
+    @IBOutlet weak var firstGeoLatLabel: UILabel!
+    @IBOutlet weak var firstGeoIngLabel: UILabel!
+    @IBOutlet weak var firstPhoneLabel: UILabel!
+    @IBOutlet weak var firstWebsiteLabel: UILabel!
+    @IBOutlet weak var firstCompanyNameLabel: UILabel!
+    @IBOutlet weak var firstCompanyPhraseLabel: UILabel!
+    @IBOutlet weak var firstCompaanyBsLable: UILabel!
+    
+    
+    
+    
+    
+    
+    
     @IBOutlet weak var tableView: UITableView!
     var persons: [Person] = []
     override func viewDidLoad() {
@@ -36,6 +58,21 @@ class ViewController: UIViewController {
                             let decoder = JSONDecoder()
                             let decodedData = try decoder.decode([Person].self, from: safeData)
                             DispatchQueue.main.async {
+                                self.idFirstLable.text = String( decodedData[0].id)
+                                self.firstNameLabel.text =  decodedData[0].name
+                                self.firstUserNameLabel.text = decodedData[0].username
+                                self.firstEmailLabel.text = decodedData[0].email
+                                self.firstAderssStreetLabel.text = decodedData[0].address.street
+                                self.firstAddressSuiteLabel.text = decodedData[0].address.suite
+                                self.firstAddressCityLabel.text = decodedData[0].address.city
+                                self.firstAddressZipLabel.text = decodedData[0].address.zipcode
+                                self.firstGeoLatLabel.text = decodedData[0].address.geo.lat
+                                self.firstGeoIngLabel.text = decodedData[0].address.geo.lng
+                                self.firstPhoneLabel.text = decodedData[0].phone
+                                self.firstWebsiteLabel.text = decodedData[0].website
+                                self.firstCompanyNameLabel.text = decodedData[0].company.name
+                                self.firstCompanyPhraseLabel.text = decodedData[0].company.catchPhrase
+                                self.firstCompaanyBsLable.text = decodedData[0].company.bs
                                 self.persons = decodedData
                                 self.tableView.reloadData()
                             }
---
+++ Bushra Barakat_Lab24/Bushra Barakat_Lab24/Controller/ViewControllerTwo.swift
@@ -0,0 +1,18 @@
+//
+//  ViewControllerTwo.swift
+//  Bushra Barakat_Lab24
+//
+//  Created by Bushra Barakat on 26/04/1443 AH.
+//
+
+import Foundation
+class ViewControllerTwo:UIViewController{
+    
+    
+    
+    
+    
+    
+    
+    
+}
