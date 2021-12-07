5d89451a7088043ec1e74d0568a1c0b8f7dbc01f
ZHOORHUSSAIN <Zhoor.Hussain.jiz.s@tuwaiq.edu.sa>
2021-12-07 09:29:47 +0300

    zhoor

---
+++ Lab24/Lab24/viewcontrollerperson.swift
@@ -0,0 +1,12 @@
+//
+//  viewcontrollerperson.swift
+//  Lab24
+//
+//  Created by زهور حسين on 23/04/1443 AH.
+//
+
+import Foundation
+import UIKit
+class viewcontrollerperson: UIViewController {
+    
+}
