968fb994688b6a812d514cd5c6aeaddeea677fc5
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2021-12-12 17:01:13 +0300

    "finally"

---
+++ GhaziShubayli_Lab24/personCell.swift
@@ -0,0 +1,14 @@
+//
+//
+import UIKit
+class TopicInfoCell: UITableViewCell {
+    @IBOutlet weak var iD: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var geo: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    @IBOutlet weak var website: UILabel!
+    @IBOutlet weak var company: UILabel!
+}
